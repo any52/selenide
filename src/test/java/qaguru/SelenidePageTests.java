e3b56645355fdbc2e90adb7630b7191847228707
Anna Moiseeva <belova_al@mail.ru>
2022-12-23 00:48:48 +0300

    Task2 was updated

--- src/test/java/qaguru/SelenidePageTests.java
+++ src/test/java/qaguru/SelenidePageTests.java
@@ -11,13 +11,12 @@ import static com.codeborne.selenide.Selenide.*;
 public class SelenidePageTests {
 
     @Test
-    void successfulSearchTest() {
+    void successfulSearchSoftAssertionsTest() {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $(byText("Show 2 more pages…")).click();
         $$(".Box-row").shouldHave(itemWithText("SoftAssertions"));
         $(byText("SoftAssertions")).click();
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
-        sleep(10000);
     }
 }
